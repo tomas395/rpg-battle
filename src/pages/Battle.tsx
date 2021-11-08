98768788421d64d5a5806f0c48c20a878b5b1c76
Will Ashe <brantashe@gmail.com>
2021-11-07 19:38:15 -0600

    pull battle UI into floating window with constrained 4/3 aspect ratio; add background image with flash effect when any hero has HURT animation active

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,26 +1,79 @@
-import styled from 'styled-components';
+import { useContext, useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 
+import { AppStateContext } from '../state';
 import EnemyGroupSection from '../components/EnemyGroupSection';
 import MainBattleSection from '../components/MainBattleSection';
 import PlayerInfoSection from '../components/PlayerInfoSection';
+import { HURT, PLAYER_GROUP } from '../constants';
 
 const BattleContainer = styled.div`
-  height: 100%;
+  margin: 0;
   display: flex;
-  flex-direction: column;
-  top: 0;
-  align-items: stretch;
-  background: #000080;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 `;
 
-const Battle = () => (
-  <>
-    <BattleContainer>
-      <EnemyGroupSection />
-      <MainBattleSection />
-      <PlayerInfoSection />
-    </BattleContainer>
-  </>
-);
+const flashAnimation = keyframes`
+  0% {
+    background-position: 0%;
+  }
+  50% {
+    background-position: -100%;
+  }
+`;
+
+const BattleWindow = styled((props: any) => <div {...props} />)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  aspect-ratio: 4 / 3;
+  width: 125vmin; // TODO: change this to 133.33vmin as soon as nav is moved out of page flow (probably dedicated modal window)
+  overflow: hidden;
+  outline: 4px solid #000088;
+
+  background: url('/assets/battle-bg.png');
+  background-size: auto 100%;
+  ${({ bgFlash }: any) => {
+    if (bgFlash) {
+      return css`
+        animation: ${flashAnimation} 0.35s steps(1) infinite;
+      `;
+    }
+  }}
+`;
+
+const Battle = () => {
+  const [state] = useContext(AppStateContext);
+  const {
+    groups: {
+      [PLAYER_GROUP]: { entities: heroes },
+    },
+  } = state;
+
+  const [bgFlash, setBgFlash] = useState(false);
+
+  useEffect(() => {
+    if (heroes.find((entity) => entity.currentAnimation.type === HURT)) {
+      setBgFlash(true);
+    } else {
+      setBgFlash(false);
+    }
+  }, [heroes]);
+
+  return (
+    <>
+      <BattleContainer>
+        <BattleWindow bgFlash={bgFlash}>
+          <EnemyGroupSection />
+          <MainBattleSection />
+          <PlayerInfoSection />
+        </BattleWindow>
+      </BattleContainer>
+    </>
+  );
+};
 
 export default Battle;
